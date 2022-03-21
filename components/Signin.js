7eb604f1d0ed9c7e79720d476101802514d19d4b
Andrey Gagauz <gagauzandrey24@gmail.com>
2022-03-21 23:07:07 +0200

    FEATURE: Added signin page

---
+++ components/Signin.js
@@ -0,0 +1,35 @@
+import styles from "../styles/Login.module.scss"
+import {useState} from "react";
+
+const Signin = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`The username is: ${username}`)
+        console.log(`The password is: ${password}`)
+    }
+
+    return (
+        <div className={styles.signin}>
+            <div className="container">
+                <h2 className={styles.heading}>Login</h2>
+                <form action="" className={styles.signin__form} onSubmit={handleSubmit}>
+                    <input type="text"
+                           value={username}
+                           placeholder="Username"
+                           onChange={(e) => setUsername(e.target.value)}/>
+                    <input type="password"
+                           value={password}
+                           placeholder="Password"
+                           onChange={(e) => setPassword(e.target.value)}/>
+
+                    <button type="submit" className={styles.btn}>Submit</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Signin;
