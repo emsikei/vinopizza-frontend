5362aa7d66367872f4da8dd076690877765203f2
Andrey Gagauz <gagauzandrey24@gmail.com>
2022-04-17 23:13:44 +0300

    UPDATE: Changes in Categories table

--- components/Dashboard/Categories/CategoriesList.js
+++ components/Dashboard/Categories/CategoriesList.js
@@ -3,6 +3,8 @@ import styles from "./Categories.module.scss";
 import {getAllUniqueCategories} from "../../../helpers";
 import menu from "../../../data/menu"
 import CategoryItem from "./CategoryItem";
+import Link from "next/link";
+import {AiOutlinePlus} from "react-icons/ai";
 
 const CategoriesList = () => {
     const [categories, setCategories] = useState(getAllUniqueCategories(menu.products))
@@ -10,6 +12,12 @@ const CategoriesList = () => {
     return (
 
         <>
+            <p style={{color: "#f6c162", paddingLeft: "100px"}}>
+                <Link href="categories/create">
+                    <a><AiOutlinePlus/><span>New category</span></a>
+                </Link>
+            </p>
+
             <table className={styles.categories}>
                 <thead>
                 <tr>
