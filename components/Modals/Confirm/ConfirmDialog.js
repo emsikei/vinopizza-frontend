1ab0c31963cbf1081f820893be3043b2deba6528
Andrey Gagauz <gagauzandrey24@gmail.com>
2022-05-01 00:16:52 +0300

    Confirm Dialog Component

--- components/Modals/Confirm/ConfirmDialog.js
+++ components/Modals/Confirm/ConfirmDialog.js
@@ -1,16 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from "./Confirm.module.scss"
+import { GrClose } from "react-icons/gr"
 
-const ConfirmDialog = ({status, item}) => {
-
-    // TODO: make api request (delete product) on "Yes" button & close dialog on "No" button
-
+const ConfirmDialog = ({ itemId, itemName, items, setItems, active, setActive, t, deleteFunc }) => {
     return (
-        <div className={`${styles.confirm} ${styles[status]}`}>
-            <p>Are you sure you want to delete the {`"${item}"`}?</p>
-            <div className={styles.buttons}>
-                <button className={styles.no}>No</button>
-                <button className={styles.yes}>Yes</button>
+        <div className={active ? `${styles.confirm} ${styles.active}` : `${styles.confirm}`}
+            onClick={() => setActive(false)
+            }>
+            <div className={active ? `${styles.confirm__content} ${styles.active}` : `${styles.confirm__content}`} onClick={e => e.stopPropagation()}>
+                <div className={styles.confirm__header}>
+                    <div className={styles.confirm__close} onClick={() => setActive(false)}>
+                        <GrClose />
+                    </div>
+                </div>
+                <div className={styles.confirm__content__inner}>
+                    <p>{t.confirmDialog.text} "{itemName}"?</p>
+                    <div className={styles.buttons}>
+                        <button className={styles.no} onClick={() => setActive(false)}>{t.confirmDialog.no}</button>
+                        <button className={styles.yes}
+                            onClick={() => deleteFunc(itemId, items, setItems, setActive)}>{t.confirmDialog.yes}</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
